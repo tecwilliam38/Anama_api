290b17e901bedef377f0af41d285927956694022
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-06-28 16:12:50 -0300

    vai

--- token.js
+++ token.js
@@ -15,7 +15,8 @@ function ValidateToken(req, res, next) {
      const authToken = req.headers['authorization'];    
      if (!authToken)
           return res.status(401).json({ error: "Token não informado" });
-     const [bearer, token] = authToken.split(" ");  // "Bearer"   "000000000"
+     // "Bearer"   "000000000"
+     const [bearer, token] = authToken.split(" ");  
      jwt.verify(token, secretToken, (err, decode) => {         
           if (err)
                return res.status(401).json({ error: "Token inválido" });
